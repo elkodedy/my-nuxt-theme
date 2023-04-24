f053e5de6f3183d3cd4993b8e5dc05a4cbef9165
“elkodedy” <elkodedy.99@gmail.com>
2023-04-24 14:30:18 +0800

    update

--- my-command.bat
+++ my-command.bat
@@ -1,22 +1,25 @@
 @echo off
 
 echo [START]
-echo.
+
 @REM echo "[PNPM GENERATE]"
 @REM pnpm generate
 
+echo.
 echo "\n [RENAME <portal.ts> TO <portal.global.ts> ] /n"
 ren "portal.ts" "portal.global.ts"
 
+echo.
 echo "git add, commit(update), and push"
 git add .
 git commit -am "update"
 git push
 
+echo.
 echo "[RENAME <portal.global.ts> TO <portal.ts> ]"
 ren "portal.global.ts" "portal.ts"
 
-
+echo.
 echo [END]
 
 pause
